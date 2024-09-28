f74bbe13eba42f15c353edd2e88c8b6c3750b97c
meherun05 <maherunn.cmt@gmail.com>
2024-09-28 23:35:24 +0600

    learning

---
+++ Module-0/hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    // int a;
+    // scanf("%d",&a);
+    // printf("a = %d",a);
+    printf("hello mebbbbbbbbbfg");
+    return 0;
+}
---
+++ Module-1/1.4.1/helloWorld.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("hello world\n");
+    printf("My Name is Meherun\n");
+    return 0 ;
+}
