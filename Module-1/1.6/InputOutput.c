5d80c5a38105869fedef562587decea0c6e75eba
meherun05 <maherunn.cmt@gmail.com>
2024-09-29 22:41:05 +0600

    module-1 update

---
+++ Module-1/1.6/InputOutput.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    double d;
+    char ch;
+    float f;
+    // scanf("%d %lf %c %f",&a, &d ,&ch);
+    // printf("a =%d d=%lf c =%c f = %f",a,d,ch, f);
+    // scanf("%f",&f);
+    // printf("%.4f", f);
+    scanf("%ld", &d);
+    printf("%ld", d);
+    return 0;
+}
+
+/*
+int = %d
+char = %c
+double = %lf
+float = %f
+long double = %Lf
+long int = %Id
+*/
