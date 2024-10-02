6949fb2eaa75e45b5aab20e4087975010f6b4c93
meherun05 <maherunn.cmt@gmail.com>
2024-10-03 00:57:35 +0600

    yesterday update

---
+++ Module-3/QuizePractice.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int main(){
+    
+  // for(int j = 5; j<=10; j++){
+  //   printf("some\n");
+  // }
+
+//   for(int i=0;i<=5;i++)
+// {
+//      for(int j=1;j<3;j++)
+//      {
+//          printf("*\n");
+//      }
+// }
+
+// int i = 0, j=0;
+// for(j=1;j<=5;j++){
+//   i++;
+// }
+// printf("%d",j);
+
+// for(int i = 0; i <=10; i++){
+//   int x = 4;
+// }
+// printf("%d",i);
+// int i = 1; 
+// do{
+//   printf("%d",i);
+//   i++;
+// }while (i<5);
+
+// int i = -5;
+// while(i < 3){
+//   printf("hello ");
+//   i+=2;
+// }
+// for(int i = 3; i>=-10; i++){
+//   printf("h");
+// }
+// int n = 10;
+// for(int i =  n -1; i > 0; i--){ //int i =  n -1; i >= 0; i--
+//   printf("%d\n",i);
+// }
+
+int i = 5;
+while(i<10){
+  printf("i\n");
+  i++;
+}
+}
