70c9617f9b26ac0d6129fc67c4434a178e99bc09
meherun05 <maherunn.cmt@gmail.com>
2025-01-27 21:42:45 +0600

    'update'

--- DSA/Final-Exam/Pairs.cpp
+++ DSA/Final-Exam/Pairs.cpp
@@ -1,36 +1,25 @@
-//  #include <bits/stdc++.h>
-
-// using namespace std;
-
-// int main(){
-//     map<string,vector<int>> m;
-//     int n;
-//     cin >> n;
-//     while(n--){
-//         string word;int x;
-//         cin >> word >> x;
-//         m[word].push_back(x);
-//     }
-//     for(auto it = m.begin(); it != m.end(); it++){
-//         string name = it->first;
-//        vector<int> val = it->second;
-//         sort(val.rbegin(), val.rend());
-
-//         for (int x : val) {
-//             cout << name << " " << x << endl;
-//         }
-//     }
-    
-//     return 0;
-// }
-
  #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    std::stack<int> s;
-    s.pop();
+    map<string,vector<int>> m;
+    int n;
+    cin >> n;
+    while(n--){
+        string word;int x;
+        cin >> word >> x;
+        m[word].push_back(x);
+    }
+    for(auto it = m.begin(); it != m.end(); it++){
+        string name = it->first;
+       vector<int> val = it->second;
+        sort(val.rbegin(), val.rend());
+
+        for (int x : val) {
+            cout << name << " " << x << endl;
+        }
+    }
     
     return 0;
 }
