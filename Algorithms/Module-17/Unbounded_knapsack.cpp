38813ff488d5422072fed3df999d5fc91c1f94af
meherun05 <maherunn.cmt@gmail.com>
2025-03-22 13:55:10 +0600

    'update'

---
+++ Algorithms/Module-17/Unbounded_knapsack.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+int val[1005],weight[1005];
+int dp[1005][1005];
+
+int unboundKnapsack(int i,int mxWeight){
+    if(i < 0 || mxWeight <= 0)
+        return 0;
+
+    if(dp[i][mxWeight] != -1){
+        return dp[i][mxWeight];
+    }
+
+     if(weight[i] <= mxWeight){
+        int op1 = unboundKnapsack(i, mxWeight - weight[i]) + val[i];
+        int op2 = unboundKnapsack(i - 1,mxWeight);
+        dp[i][mxWeight] = max(op1,op2);
+        return dp[i][mxWeight];
+    }else{
+        dp[i][mxWeight] = unboundKnapsack(i - 1,mxWeight);
+        return dp[i][mxWeight];
+    }
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int mxWeight;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i];
+    }
+    cin >> mxWeight;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= mxWeight; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
+    cout << unboundKnapsack(n-1,mxWeight) << endl;
+    
+        
+
+    return 0;
+}
